f184f47b40e2f449db6b59a306b269079f5c5fbe
Sam Chiu <samchiu064@gmail.com>
2022-03-25 22:38:46 +0800

    Working on Thermos classic page

--- src/views/StoreThermosClassic.vue
+++ src/views/StoreThermosClassic.vue
@@ -1,18 +1,14 @@
 <template>
   <main class="showcase position-relative">
     <div class="container-fluid bg-light h-100">
-      <div class="row h-100">
-        <div class="col-12 h-100">
+      <div class="row h-100 p-0 p-0 m-0">
+        <div class="col-12 h-100 p-0">
           <article class="showcase__product h-100">
-            <div class="product col-2 order-3">
-              <img src="../assets/showcase_hookers_green.png" alt="胡克綠保溫瓶" />
-              <!-- <button type="button" class="btn btn-primary">立即購買</button> -->
-            </div>
-            <div class="feature col-4 order-2">
-              <div class="line">
-                <span></span>
-              </div>
-              <div class="text">
+            <section class="feature col-4 order-2">
+              <div class="description">
+                <!-- <div class="line">
+                  <span></span>
+                </div> -->
                 <h2>
                   第一行文字字 <br />
                   第二行文字 <br />
@@ -25,14 +21,32 @@
                 </p>
                 <span>※黑空確，友市才部這的我的你是友流子去班終以主這道他好空灣為更</span>
               </div>
-            </div>
-            <div class="accessory col-4 order-4">Accessory</div>
-            <div class="decoration col-1 order-1">
-              <!-- Decoration -->
               <img src="../assets/bottle_open_hookers_green.png" alt="胡克綠保溫瓶" />
-            </div>
-            <div class="benefit col-1 order-5">benefit</div>
+            </section>
+            <section class="product col-4 order-3">
+              <img src="../assets/showcase_hookers_green.png" alt="胡克綠保溫瓶" />
+              <!-- <button type="button" class="btn btn-primary">立即購買</button> -->
+            </section>
+            <section class="accessory col-4 order-4">
+              <img src="../assets/bottle_cap_hookers_green.png" alt="胡克綠保溫瓶瓶蓋" />
+              <div class="description">
+                <!-- <div class="line">
+                  <span></span>
+                </div> -->
+                <h2>
+                  第一行文字字文字 <br />
+                  第二行文字文字文字文字 <br />
+                </h2>
+                <p>黑空確，友市才部這的我的你是友流子去班終以主這道他好</p>
+                <span>※黑空確，友市才部這的我的你是友流子去班終以主這道他好空灣為更</span>
+              </div>
+              <div class="benefit">
+                <p>※最長可保溫24小時</p>
+                <img src="../assets/benefit_hookers_green.png" alt="胡克綠保溫瓶保溫效果圖" />
+              </div>
+            </section>
           </article>
+          <div class="switch-nav"></div>
         </div>
       </div>
     </div>
@@ -49,14 +63,17 @@ export default {};
   padding: 0;
 }
 
+::-moz-selection {
+  /* Code for Firefox */
+  background: yellow;
+}
+
+::selection {
+  background: yellow;
+}
+
+// Custom
 .showcase {
-  background: rgb(248, 249, 250);
-  background: radial-gradient(
-    circle,
-    rgba(248, 249, 250, 1) 13%,
-    rgba(255, 255, 255, 1) 27%,
-    rgba(248, 249, 250, 1) 42%
-  );
   color: #383838;
   height: calc(100vh - 5.25rem);
   @media (max-width: 992px) {
@@ -64,58 +81,126 @@ export default {};
   }
   &__product {
     display: flex;
-    div {
-      // width: 300px;
-      // height: 200px;
-      background-color: seagreen;
+    background: rgb(248, 249, 250);
+    background: radial-gradient(
+      circle,
+      rgba(248, 249, 250, 1) 13%,
+      rgba(255, 255, 255, 1) 27%,
+      rgba(248, 249, 250, 1) 42%
+    );
+    section {
+      // background-color: seagreen;
       // border: 1px solid black;
-      margin: 1px;
+      // margin: 1px;
       text-align: left;
-      &.decoration {
-        align-self: end;
-        z-index: 1;
-      }
       &.feature {
         margin-top: 70px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        .description {
+          margin-left: 12vw;
+          h2 {
+            position: relative;
+            text-decoration: underline;
+            text-underline-offset: 0.1em;
+            line-height: 2.875rem;
+            &::before {
+              font-size: 0.8rem;
+              display: flex;
+              width: 4.3125rem;
+              height: 4.3125rem;
+              background-color: #4d6d58;
+              border-radius: 50%;
+              position: absolute;
+              left: -80px;
+              content: '臺灣MIT微笑標章';
+              color: white;
+              text-align: center;
+              align-items: center;
+              line-height: 1.1rem;
+              font-weight: 300;
+            }
+          }
+          p {
+            font-weight: 300;
+          }
+          span {
+            font-weight: 300;
+          }
+        }
+        .line {
+          span {
+            display: block;
+            width: 14vw;
+            border: 1px #383838 dashed;
+            transform: rotate(21deg);
+            position: relative;
+            left: 19vw;
+            top: 12vh;
+          }
+        }
+        img {
+          max-width: 27.8vw;
+        }
       }
       &.product {
-        margin-top: 90px;
-        text-align: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        img {
+          max-width: 100%;
+          height: auto;
+        }
       }
       &.accessory {
-        //   margin-top: 500px;
-      }
-      // &.benefit {
-      //   align-self: end;
-      //   margin-bottom: 120px;
-      // }
-    }
-    .product {
-      // top: 45%;
-    }
-    .feature {
-      // position: absolute;
-      // left: 10%;
-      // top: 8%;
-      .line {
-        span {
-          display: block;
-          width: 200px;
-          border: 2px brown dashed;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        .line {
+          span {
+            display: block;
+            width: 20vh;
+            border: 1px #383838 dashed;
+            position: relative;
+            left: 12vw;
+            bottom: 5.5vw;
+            transform: rotate(90deg);
+          }
         }
-      }
-      .text {
-        h2 {
-          text-decoration: underline;
-          text-underline-offset: 0.1em;
-          line-height: 2.875rem;
-          // border-bottom: 1px solid #000;
+        .description {
+          margin-top: 4rem;
+          h2 {
+            text-decoration: underline;
+            text-underline-offset: 0.1em;
+            line-height: 2.875rem;
+          }
+          p {
+            font-weight: 300;
+          }
+          span {
+            font-weight: 300;
+          }
         }
-        p {
-          font-weight: 300;
+        & > img {
+          max-width: 20vw;
+          height: auto;
+          align-self: flex-end;
+          position: relative;
+          right: 5vw;
+          top: 1vw;
         }
-        span {
-          font-weight: 300;
+        .benefit {
+          align-self: flex-end;
+          position: relative;
+          right: 2vw;
+          bottom: 3vw;
+          display: flex;
+          align-items: flex-end;
+          img {
+            max-width: 14vw;
+            height: auto;
+          }
         }
       }
     }
