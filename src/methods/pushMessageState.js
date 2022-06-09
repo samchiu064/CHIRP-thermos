cba93c839d8164e7070b658b89777643e3789b15
Sam Chiu <samchiu064@gmail.com>
2022-06-10 00:00:52 +0800

    Moved comments place.

--- src/methods/pushMessageState.js
+++ src/methods/pushMessageState.js
@@ -2,15 +2,14 @@ import emitter from './emitter';
 
 export default function pushMessage(res, title = '更新') {
   if (!res.data.success) {
-    // 將訊息統一為 array for array.join() method
-    const message = typeof res.data.message === 'string' ? [res.data.message] : res.data.message;
-    emitter.emit('push-messages', {
+    const message = typeof res.data.message === 'string' ? [res.data.message] : res.data.message; // 將訊息統一為 array for array.join() method
+    emitter.emit('pushMessages', {
       status: 'failed',
       title: `${title}失敗`,
       content: message.join('、'),
     });
   } else {
-    emitter.emit('push-messages', {
+    emitter.emit('pushMessages', {
       status: 'success',
       title: `${title}成功`,
     });
