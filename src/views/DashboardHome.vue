8f572cb176d56ee577786d2e0cdaa88a8ef96911
Sam Chiu <samchiu064@gmail.com>
2022-06-13 15:48:11 +0800

    Added the template of dashboardHome page. Adjuste StoreCheckOrder to StoreOrderCheck.

--- src/views/DashboardHome.vue
+++ src/views/DashboardHome.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="container-fluid mt-3 position-relative"></div>
+  <div class="mt-6">
+    <h1 class="fw-regular text-center fs-4">
+      歡迎來到CHIRP鳥牌後臺管理系統，請點選上方導覽列或查看下方頁面說明 。
+    </h1>
+    <div class="card-group justify-content-center mt-4">
+      <div class="card text-dark bg-light" style="max-width: 18rem">
+        <div class="card-header">產品</div>
+        <div class="card-body">
+          <h5 class="card-title">新增/編輯/刪除產品</h5>
+          <p class="card-text">
+            商品標題、分類、單位、原價、售價、產品描述、說明內容及產品圖片的新增、編輯及刪除。
+          </p>
+        </div>
+      </div>
+      <div class="card text-dark bg-light" style="max-width: 18rem">
+        <div class="card-header">訂單</div>
+        <div class="card-body">
+          <h5 class="card-title">編輯訂單狀態/刪除訂單</h5>
+          <p class="card-text">訂單付款狀態的編輯及個別訂單的刪除。</p>
+        </div>
+      </div>
+      <div class="card text-dark bg-light" style="max-width: 18rem">
+        <div class="card-header">優惠券</div>
+        <div class="card-body">
+          <h5 class="card-title">新增/編輯/刪除優惠券</h5>
+          <p class="card-text">
+            優惠券名稱、折扣百分比、到期日、折扣碼、是否啟用的新增、編輯及刪除。
+          </p>
+        </div>
+      </div>
+      <div class="card text-dark bg-light" style="max-width: 18rem">
+        <div class="card-header">登入、登出</div>
+        <div class="card-body">
+          <h5 class="card-title">管理員登入/登出</h5>
+          <p class="card-text">帳號密碼登入及登出功能，需登入後才可進入產品、訂單、優惠券頁面。</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
---
+++ src/views/StoreOrderCheck.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center mt-6">
+      <div class="col col-lg-6">
+        <p class="text-center fs-5 fw-medium">請輸入您的 20 碼訂單號碼以獲取訂單資訊</p>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col col-lg-6">
+        <div class="input-group">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="訂單號碼"
+            aria-label="訂單號碼"
+            aria-describedby="button-addon2"
+            maxlength="20"
+            v-model="orderId"
+          />
+          <button
+            class="btn btn-outline-secondary"
+            type="button"
+            id="button-addon2"
+            @click="getOrderList(orderId)"
+          >
+            查詢
+          </button>
+        </div>
+        <span
+          v-if="!orderIsValid && orderId.length !== 0 && orderId.length < 20"
+          class="text-danger fs-7 ps-3 m-0"
+          >無效的訂單號碼，請輸入 20 個字元
+        </span>
+      </div>
+    </div>
+    <div class="row justify-content-center m-0 mt-4">
+      <div class="col col-lg-6">
+        <StoreUserCartOrderList v-if="orderIsValid" :order="order" />
+      </div>
+    </div>
+    <div class="row justify-content-center m-0">
+      <div class="col col-lg-6">
+        <StoreUserCartOrderTable v-if="orderIsValid" :form="form" />
+      </div>
+    </div>
+  </div>
+
+  <StoreFooter />
+</template>
+
+<script>
+import StoreUserCartOrderList from '@/components/StoreUserCartOrderList.vue';
+import StoreUserCartOrderTable from '@/components/StoreUserCartOrderTable.vue';
+import StoreFooter from '@/components/StoreFooter.vue';
+import { apiGetOrderListById } from '@/api/client';
+
+export default {
+  components: {
+    StoreUserCartOrderList,
+    StoreUserCartOrderTable,
+    StoreFooter,
+  },
+  data() {
+    return {
+      order: {},
+      orderId: '',
+      orderIsValid: false,
+    };
+  },
+  computed: {
+    form() {
+      const form = {};
+      form.message = this.order.message;
+      form.user = { ...this.order.user };
+      return form;
+    },
+  },
+  methods: {
+    async getOrderList(orderId) {
+      if (orderId.length < 20) {
+        this.orderIsValid = false;
+        return;
+      }
+      this.$Progress.start();
+      await apiGetOrderListById(orderId)
+        .then((res) => {
+          if (res.data.success === true) {
+            this.order = res.data.order;
+            this.orderIsValid = true;
+            this.$Progress.finish();
+          }
+          console.log(res);
+        })
+        .catch((res) => {
+          this.$Progress.fail();
+          console.log(res);
+        });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.form-control {
+  background-color: transparent;
+}
+</style>
