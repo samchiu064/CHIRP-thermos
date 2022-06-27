b86039ac9e9129b50c47c61007729e695ca85b33
Sam Chiu <samchiu064@gmail.com>
2022-06-27 17:55:16 +0800

    Added a new folder 'scss' under assets folder. Updated App.vue for the new path of all.scss.

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,6 @@ export default {};
 </script>
 
 <style lang="scss">
-@import './assets/all.scss';
+@import './assets/scss/all.scss';
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap');
 </style>
