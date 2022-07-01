5f65821af1c46950db277c0917c7c4ac8aeb837e
Sam Chiu <samchiu064@gmail.com>
2022-07-01 22:39:00 +0800

    Added new design to checkout success page.

--- src/views/StoreUserCartCheckoutOrder.vue
+++ src/views/StoreUserCartCheckoutOrder.vue
@@ -1,53 +1,112 @@
 <template>
+  <div class="row">
+    <div class="col text-center">
+      <div class="bottle-outline">
+        <img src="@/assets/images/static/bottle_outline.svg" alt="" class="bottle-outline__img" />
+      </div>
+    </div>
+  </div>
   <div class="row py-2">
     <div class="col">
+      <p class="fs-4 text-center mb-0">購買完成</p>
       <p class="fs-4 text-center">
-        <i class="bi bi-check2-circle text-success"></i> 感謝您的購買，您的訂單
-        <span class="text-success">{{ order.id }}</span> 已建立成功
+        您的訂單 <span class="text-success">{{ order.id }}</span> 已建立成功
+      </p>
+      <p class="text-center">
+        接下來您可以考慮訂閱我們，或是
+        <router-link
+          to="/thermos/classic/details/classic-green"
+          class="text-decoration-none link-classic-green"
+          >繼續購物</router-link
+        >
       </p>
     </div>
   </div>
-  <div class="row">
+  <div class="row mb-5">
     <div class="col-lg-6 m-auto">
-      <form class="subscription-box">
-        <p>訂閱電子報</p>
-        <label for="email" class="form-label col-12 px-4 mb-3"
-          >Email<i class="asterisk"></i>
+      <VForm @submit="subscribe" class="subscription-box p-3">
+        <p class="ps-2 pb-3 mb-0 fw-bold">訂閱電子報</p>
+        <p class="ps-2">輸入MAIL訂閱電子報，及時獲取最新的商品資訊！</p>
+        <div class="input-group mb-0">
           <VField
-            name="Email欄位"
+            name="Email"
             type="email"
-            rules="email|required"
-            class="form-control mt-1"
+            rules="email"
+            class="form-control ms-2 bg-transparent"
             id="email"
-            placeholder="請填入要被通知的Email"
+            placeholder="Email"
+            :disabled="isSubscribed"
           />
-          <ErrorMessage name="Email欄位" class="text-danger fs-7 ps-2 m-0" />
-          <button type="submit" class="btn btn-primary" @click.prevent>Submit</button>
-        </label>
-      </form>
+          <button
+            v-if="isSubscribed === false"
+            class="btn btn-outline-secondary me-3"
+            type="submit"
+          >
+            訂閱
+          </button>
+          <button
+            v-else
+            class="btn btn-outline-success me-3"
+            type="button"
+            id="button-addon2"
+            disabled
+          >
+            訂閱成功
+          </button>
+        </div>
+        <ErrorMessage name="Email" class="text-danger fs-7 ms-3" />
+      </VForm>
     </div>
   </div>
   <div class="row">
     <div class="col-lg-6 m-auto">
-      <StoreUserCartOrderList :order="order" />
-      <StoreUserCartOrderTable :form="tempForm" />
-    </div>
-  </div>
-
-  <div class="row justify-content-center mt-3">
-    <div class="col-5 col-lg-3">
-      <button
-        type="button"
-        class="btn btn-outline-secondary rounded-pill w-100 py-2"
-        @click="this.$router.push({ name: 'OrderCheck' })"
-      >
-        查詢訂單
-      </button>
-    </div>
-    <div class="col-5 col-lg-3">
-      <button type="button" class="btn btn-dark rounded-pill w-100 py-2" @click="printWindow">
-        列印此頁
-      </button>
+      <div class="accordion-collapse" id="accordionOrder">
+        <div class="accordion-item bg-transparent">
+          <h2 class="accordion-header" id="headingOne">
+            <button
+              class="accordion-button bg-transparent text-dark fw-bold"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
+            >
+              訂單詳情
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            class="accordion-collapse collapse show"
+            aria-labelledby="headingOne"
+            data-bs-parent="#accordionOrder"
+          >
+            <div class="accordion-body">
+              <StoreUserCartOrderList :order="order" />
+              <StoreUserCartOrderTable :form="tempForm" />
+              <div class="row justify-content-center mt-3">
+                <div class="col-5 col-lg-4">
+                  <button
+                    type="button"
+                    class="btn btn-outline-secondary rounded-pill w-100 py-2"
+                    @click="this.$router.push({ name: 'OrderCheck' })"
+                  >
+                    查詢訂單
+                  </button>
+                </div>
+                <div class="col-5 col-lg-4">
+                  <button
+                    type="button"
+                    class="btn btn-dark rounded-pill w-100 py-2"
+                    @click="printWindow"
+                  >
+                    列印此頁
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -77,19 +136,74 @@ export default {
       default: '',
     },
   },
+  data() {
+    return {
+      isSubscribed: false,
+      subscriberEmail: '',
+    };
+  },
   emits: ['getOrderList'],
   methods: {
     printWindow() {
       window.print();
     },
+    subscribe(values) {
+      if (values.Email) {
+        this.subscriberEmail = values.Email;
+        this.isSubscribed = true;
+      }
+    },
     ...mapActions(useCartStore, ['getCartList']),
   },
   created() {
     this.$Progress.start();
-    // Update the information of cart & order to the latest
     this.getCartList();
     this.$emit('getOrderList', this.$route.params.orderId);
     this.$Progress.finish();
   },
 };
 </script>
+
+<style lang="scss">
+.bottle-outline {
+  max-width: 65px;
+  max-height: 200px;
+  position: relative;
+  overflow: hidden;
+  margin: auto;
+  &__img {
+    max-width: 100%;
+    max-height: 100%;
+  }
+
+  &:before {
+    content: '';
+    position: absolute;
+    width: 210px;
+    height: 210px;
+    background: #cc7e85;
+    left: 50%;
+    bottom: 0%;
+    transform: translateX(-50%);
+    border-radius: 40%;
+    animation: fill 4s;
+    z-index: -1;
+  }
+}
+
+.subscription-box {
+  background-color: transparent;
+  border: 1px solid #dee2e6;
+}
+
+@keyframes fill {
+  from {
+    bottom: -200px;
+    transform: translateX(-50%) rotate(0deg);
+  }
+  to {
+    bottom: 0px;
+    transform: translateX(-50%) rotate(360deg);
+  }
+}
+</style>
