4b28abfa8de855c8ed22efbd467e279bbc7fb1dc
Sam Chiu <samchiu064@gmail.com>
2022-07-01 22:37:52 +0800

    Changed wording of product card.

--- src/components/StoreProductCard.vue
+++ src/components/StoreProductCard.vue
@@ -7,7 +7,7 @@
           <img :src="item.imageUrl" class="img-box rounded w-100" alt="商品圖片" />
           <div class="overlay w-100 h-100 position-absolute top-0 start-0">
             <div class="calendar-box position-absolute top-50 start-50 translate-middle">
-              <p class="text-center my-1">上市時通知我</p>
+              <p class="text-center my-1">上架時通知我</p>
               <div class="calendar-links d-flex">
                 <a
                   href="https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20220729%2F20220730&details=%E9%97%9C%E6%B3%A8%E6%88%91%E5%80%91%3A%20https%3A%2F%2Fsamchiu064.github.io%2FCHIRP-thermos%2F%23%2F&text=CHIRP%20%E9%B3%A5%E7%89%8C%E4%BF%9D%E6%BA%AB%E7%93%B6%E6%96%B0%E5%93%81%E4%B8%8A%E6%9E%B6%20-%20%E6%96%B0%E4%B8%80%E4%BB%A3%E4%BF%9D%E5%86%B7%E7%93%B6%20-%20%E7%B9%BD%E7%B4%9B%E8%89%B2%E5%BD%A9"
