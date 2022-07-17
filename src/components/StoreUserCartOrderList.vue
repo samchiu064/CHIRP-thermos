d990988921c64c88cf1a761b66bd36bb40a812a4
Sam Chiu <samchiu064@gmail.com>
2022-07-17 22:34:33 +0800

    Adjusted table to responsive table. Adjusted layout for sum.

--- src/components/StoreUserCartOrderList.vue
+++ src/components/StoreUserCartOrderList.vue
@@ -1,24 +1,28 @@
 <template>
-  <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">品名</th>
-        <th scope="col">數量</th>
-        <th scope="col" class="text-end">單價</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="item in order.products" :key="item.id">
-        <td scope="row">{{ item.product.title }}</td>
-        <td>{{ item.qty }}/{{ item.product.unit }}</td>
-        <td class="text-end">{{ item.product.price?.toLocaleString('en-us') }}</td>
-      </tr>
-    </tbody>
-    <tfoot class="text-end">
-      <th scope="row" colspan="2">總價</th>
-      <td>NT$ {{ order.total?.toLocaleString('en-us') }}</td>
-    </tfoot>
-  </table>
+  <div class="table-responsive">
+    <table class="table text-nowrap">
+      <thead>
+        <tr>
+          <th scope="col">品名</th>
+          <th scope="col">數量</th>
+          <th scope="col" class="text-end">單價</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in order.products" :key="item.id">
+          <td scope="row">{{ item.product.title }}</td>
+          <td>{{ item.qty }}/{{ item.product.unit }}</td>
+          <td class="text-end">{{ item.product.price?.toLocaleString('en-us') }}</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <th scope="row">總價</th>
+          <td colspan="2" class="text-end">NT$ {{ order.total?.toLocaleString('en-us') }}</td>
+        </tr>
+      </tfoot>
+    </table>
+  </div>
 </template>
 
 <script>
