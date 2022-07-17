e4e4422b255b40871db01fd2467836c483b3cacd
Sam Chiu <samchiu064@gmail.com>
2022-07-17 22:51:31 +0800

    Updated deply.sh

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-git push -f git@github.com/samchiu064/CHIRP-thermos.git master:gh-pages
+git push -f git@github.com:samchiu064/CHIRP-thermos.git master:gh-pages
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
